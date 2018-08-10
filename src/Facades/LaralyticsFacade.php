1c93cb71240bc88122ce18dc312064879747e1fd
Michael Burton <me@mikeylicio.us>
2018-08-10 20:52:50 +0100

    Some more boilerplate and PHPDoc

--- src/Facades/LaralyticsFacade.php
+++ src/Facades/LaralyticsFacade.php
@@ -3,6 +3,16 @@ namespace MadMikeyB\Laralytics\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Laralytics
+ *
+ * @package    MadMikeyB\Laralytics
+ * @author     Michael Burton <me@mikeylicio.us>
+ * @copyright  2018 Michael Burton
+ * @license    MIT https://opensource.org/licenses/MIT
+ * @since      0.0.1
+ * @link       https://github.com/madmikeyb/laravel-analytics
+ */
 class LaralyticsFacade extends Facade
 {
     /**
